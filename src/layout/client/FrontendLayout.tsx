9fc10d7c75cdb14747d487b3cc2fc51d25a9ae02
Muneeb Mughal <muneeb.mughal@abendsoft.com>
2023-07-20 23:40:32 -0700

    fetch prodduct using api

--- src/layout/client/FrontendLayout.tsx
+++ src/layout/client/FrontendLayout.tsx
@@ -1,9 +1,7 @@
 import { Outlet } from 'react-router-dom';
-// import { Navbar } from '@src/layout/client/Navbar';
 export const FrontendLayout = () => {
   return (
     <div className='app'>
-      {/* <Navbar /> */}
       <div className='client_content'>
         <Outlet />
       </div>
